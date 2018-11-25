ead1b115003d7a93c64d3645a78694b1f81d5f6e
Felipe Gavilan <elmejor_887@hotmail.com>
2018-11-25 11:33:49 -0400

    Added new IHostedService to consume EF Core DbContext from it

---
+++ IHostedServiceDemo/Services/ConsumeScopedService.cs
@@ -0,0 +1,53 @@
+﻿using IHostedServiceDemo.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class ConsumeScopedService : IHostedService, IDisposable
+    {
+        public ConsumeScopedService(IServiceProvider services)
+        {
+            Services = services;
+        }
+
+        private Timer _timer;
+
+        public IServiceProvider Services { get; }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(20));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            using (var scope = Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var message = "ConsumeScopedService. Received message at " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+                var log = new HostedServiceLog() { Message = message };
+                context.HostedServiceLogs.Add(log);
+                context.SaveChanges();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+
+}
--- IHostedServiceDemo/Startup.cs
+++ IHostedServiceDemo/Startup.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IHostedServiceDemo.Models;
 using IHostedServiceDemo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -25,7 +27,9 @@ namespace IHostedServiceDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddTransient<Microsoft.Extensions.Hosting.IHostedService, ConsumeScopedService>();
             services.AddTransient<Microsoft.Extensions.Hosting.IHostedService, WriteToFileHostedService>();
             services.AddTransient<Microsoft.Extensions.Hosting.IHostedService, WriteToFileHostedService2>();
 
