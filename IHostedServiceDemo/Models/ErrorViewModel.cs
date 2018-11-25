493a99c5eec94ecfbbd21d81edd86b24217cf10b
Felipe Gavilan <elmejor_887@hotmail.com>
2018-11-25 10:40:18 -0400

    Add project files.

---
+++ IHostedServiceDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IHostedServiceDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IHostedServiceDemo/Services/WriteToFileHostedService2.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService2 : IHostedService, IDisposable
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string fileName = "File 2.txt";
+        private Timer timer;
+
+        public WriteToFileHostedService2(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService2: Process Started");
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(7));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            WriteToFile("WriteToFileHostedService2: Doing some work at " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService2: Process Stopped");
+            timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+
+}
